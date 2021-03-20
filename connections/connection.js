f48431d7709465c52cfdc908ac9446d70be3bf9c
Justin Green <JGreen72181@hotmail.com>
2021-03-20 08:23:58 -0700

    push

--- connections/connection.js
+++ connections/connection.js
@@ -1,21 +1,22 @@
 const mysql = require('mysql');
-let connection={}
 
-if(process.env.JAWSDB_URL){
+// if(process.env.JAWSDB_URL){
 
- connection = process.env.JAWSDB_URL
+//  connection = process.env.JAWSDB_URL
  
-}
+// }
 
-else{
-  connection=mysql.createConnection({
-    port:8889,
-    host:"localhost",
-    database:'weedstoredb',
-    user:'root',
-    password:'root',
-  })
-}
+// else{
+//   connection=mysql.createConnection({
+//     port:8889,
+//     host:"localhost",
+//     database:'weedstoredb',
+//     user:'root',
+//     password:'root',
+//   })
+// }
+
+let connection = 'mysql://skd9iqj2225a000e:onamvytpzcvf1k86@xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/m486dkyp7sg4d0e1'
 
 
 
